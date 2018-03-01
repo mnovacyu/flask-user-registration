b786dc6606926756996608a07aaff7e6b5dd03b1
Michael Novac-Yu <michael.novacyu@teamtreehouse.com>
2018-03-01 15:14:31 -0800

    add articles table

--- script.sh
+++ script.sh
@@ -25,8 +25,11 @@ sudo pip3.6 install passlib
 sudo yum -y install mariadb-server mysql-devel
 sudo systemctl start mariadb
 
-# Setup databases
+# Creat database
 mysql -e "CREATE DATABASE myflaskapp"
 
+# Create users table
 mysql -e "USE myflaskapp; CREATE TABLE users(id INT(11) AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), email VARCHAR(100), username VARCHAR(30), password VARCHAR(100), register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"
 
+# Create articles table
+mysql -e "USE myflaskapp; CREATE TABLE articles(id INT(11) AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), author VARCHAR(100), body TEXT, create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP);"
