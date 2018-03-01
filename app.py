b445b466163327342913b6b426985be50c9841d9
Michael Novac-Yu <michael.novacyu@teamtreehouse.com>
2018-02-28 16:00:38 -0800

    add login, logout, and dashboard pages

--- app.py
+++ app.py
@@ -12,27 +12,33 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'myflaskapp'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
 # Init MySQL
 mysql = MySQL(app)
 
 Articles = Articles()
 
+# Index
 @app.route('/')
 def index():
     return render_template('home.html')
 
+# About
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+# Articles
 @app.route('/articles')
 def articles():
     return render_template('articles.html', articles = Articles)
 
+# Single Article
 @app.route('/article/<string:id>/')
 def article(id):
     return render_template('article.html', id=id)
 
+# Register Form Class
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
     username = StringField('Username', [validators.Length(min=4, max=25)])
@@ -67,6 +73,55 @@ def register():
 
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
+
+# User Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Get form fields
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Get user by username
+        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+
+        if result > 0:
+            # Get stored hash
+            data = cur.fetchone()
+            password = data['password']
+
+            # Compare passwords
+            if sha256_crypt.verify(password_candidate, password):
+                # Passed
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('You are now logged in', 'success')
+                return redirect(url_for('dashboard'))
+            else:
+                error = 'Invalid login'
+                return render_template('login.html', error=error)      
+        else:
+            error = 'Username not found'
+            return render_template('login.html', error=error) 
+        # Close connection
+        cur.close()
+    return render_template('login.html')
+
+# Logout
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+    return redirect(url_for('login'))
+
+# Dashboard
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.secret_key='secret123' #session key
