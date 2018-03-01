577f9ee2b55516ca58208713ce6a131ad59ae5e5
Michael Novac-Yu <michael.novacyu@teamtreehouse.com>
2018-02-28 16:20:07 -0800

    add wrapper on validating session login for dashboard

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -111,6 +112,17 @@ def login():
         cur.close()
     return render_template('login.html')
 
+# Check if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
 # Logout
 @app.route('/logout')
 def logout():
@@ -120,6 +132,7 @@ def logout():
 
 # Dashboard
 @app.route('/dashboard')
+@is_logged_in
 def dashboard():
     return render_template('dashboard.html')
 
